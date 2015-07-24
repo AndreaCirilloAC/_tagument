e4530c0e120d4e08c25fb171352a6cecba31743a
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-24 23:12:07 +0200

    Initial Commit

---
+++ tagument/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tagument
+//
+//  Created by Andrea on 24/07/15.
+//  Copyright (c) 2015 AndreaCirilloAC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
